7f64f986b8318a9738e14f33cb4b722e10782cdd
Leo Lee <ukal5006@naver.com>
2024-11-21 14:10:37 +0900

    feat : 거리에 따라 응급실 필터링 구현

--- develop/src/components/EroomList.tsx
+++ develop/src/components/EroomList.tsx
@@ -12,18 +12,41 @@ interface DistanceEroomInfo extends EroomInfo {
 }
 
 function EroomList({ userLocation }: LocationProps) {
+    const [maxDistance, setMaxDistance] = useState(20);
     const [distanceEroomList, setDistanceEroomList] = useState<null | DistanceEroomInfo[]>(null);
+    const [filtedEroomList, setFiltedEroomList] = useState<null | DistanceEroomInfo[]>(null);
 
     useEffect(() => {
         setDistanceEroomList(
             eroomInfoList.map((eroomInfo) => {
+                const distanceValue = distance(
+                    userLocation.longitude,
+                    userLocation.latitude,
+                    eroomInfo.lon,
+                    eroomInfo.lat
+                );
+
                 return {
                     ...eroomInfo,
-                    distance: distance(userLocation.longitude, userLocation.latitude, eroomInfo.lon, eroomInfo.lat), // distance 속성 추가
+                    distance: distanceValue, // distance 속성 추가
                 };
             })
         );
     }, [userLocation]);
+
+    useEffect(() => {
+        if (distanceEroomList) {
+            setFiltedEroomList(
+                distanceEroomList.filter((distanceEroomInfo) => distanceEroomInfo.distance < maxDistance) // 거리에 따라 필터링
+            );
+        }
+    }, [distanceEroomList, maxDistance]);
+
+    useEffect(() => {
+        if (filtedEroomList) {
+            console.log(filtedEroomList);
+        }
+    }, [filtedEroomList]);
 
     return <></>;
 }
